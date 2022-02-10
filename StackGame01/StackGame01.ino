74c9a553245bc01605f52cd366dfd2e12ab19e54
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-02-10 14:44:40 +0900

    プログラムを明示的に変更

--- StackGame01/StackGame01.ino
+++ StackGame01/StackGame01.ino
@@ -3,7 +3,8 @@
    圧力をシリアルモニタに表示しよう
 */
 
-int power = 0; // 圧力センサが計測する力
+const int forcePin = A0; // 圧力センサに接続されたpin
+int force = 0; // 圧力センサが計測する力
 
 // 初期設定:最初に1回だけ実行する
 void setup() {
@@ -12,7 +13,7 @@ void setup() {
 
 // ずっと繰り返し実行する
 void loop() {
-  power = analogRead(0); // アナログピン0番から値を読み取る．
-  Serial.println(power); // シリアルポートにpowerを出力する．printlnは改行あり
+  force = analogRead(forcePin); // アナログピン0番から値を読み取る．
+  Serial.println(force); // シリアルポートにforce出力する．printlnは改行あり
   delay(1000); // 1000ms（1秒）待つ
 }
