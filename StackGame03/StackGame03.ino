842f833fc015d2212d347baef0ebe2f87b546ad0
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-02-10 16:39:41 +0900

    add random

--- StackGame03/StackGame03.ino
+++ StackGame03/StackGame03.ino
@@ -7,18 +7,20 @@
 const int ledPin = 9; // LEDを9ピンに接続
 const int forcePin = A0; // 圧力センサをアナログ0ピンに接続
 int force = 0; // 圧力センサが計測する力
+int limit; // LEDが点灯する時の圧力センサの値
 
 // 初期設定:最初に1回だけ実行する
 void setup() {
   Serial.begin(9600); // シリアルポートの設定
   pinMode(ledPin, OUTPUT); // LEDを出力に設定
+  limit = random(180,230); // 180~229の間でランダムに決定しLEDが点灯する圧力を変える
 }
 
 // ずっと繰り返し実行する
 void loop() {
   force = analogRead(forcePin); // アナログピン0番から値を読み取る．
   Serial.println(force); // シリアルポートにforce出力する．printlnは改行あり
-  if(force > 200){
+  if(force > limit){
     digitalWrite(ledPin, HIGH); // LEDを点灯
   }else{
     digitalWrite(ledPin, LOW); // LEDを消灯
