b6bef93d122031a7bbae1af14cfb2e32885f8746
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-02-10 16:34:15 +0900

    Done 実践編

---
+++ StackGame03/StackGame03.ino
@@ -0,0 +1,27 @@
+/*積み上げゲームをつくろう その3
+   内容
+   閾値を超えると，LEDが点灯する
+   ルール:容器にビー玉を交互に入れて，LEDが光ったら負け
+*/
+
+const int ledPin = 9; // LEDを9ピンに接続
+const int forcePin = A0; // 圧力センサをアナログ0ピンに接続
+int force = 0; // 圧力センサが計測する力
+
+// 初期設定:最初に1回だけ実行する
+void setup() {
+  Serial.begin(9600); // シリアルポートの設定
+  pinMode(ledPin, OUTPUT); // LEDを出力に設定
+}
+
+// ずっと繰り返し実行する
+void loop() {
+  force = analogRead(forcePin); // アナログピン0番から値を読み取る．
+  Serial.println(force); // シリアルポートにforce出力する．printlnは改行あり
+  if(force > 200){
+    digitalWrite(ledPin, HIGH); // LEDを点灯
+  }else{
+    digitalWrite(ledPin, LOW); // LEDを消灯
+  }
+  delay(10); // 10ms（00.1秒）待つ
+}
